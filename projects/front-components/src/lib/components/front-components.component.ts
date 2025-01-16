c5de0a700354cb35d5089e219eb2206355beab45
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-01-16 13:14:24 +0300

    chore: initialize project

---
+++ projects/front-components/src/lib/components/front-components.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snab-front-components',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      front-components works!
+    </p>
+  `,
+  styles: ``
+})
+export class FrontComponentsComponent {
+
+}
