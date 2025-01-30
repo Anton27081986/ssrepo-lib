0ead60fa65d4b0199bf42e4b1042c6b468d5fc2f
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-01-30 17:29:31 +0300

    feature/2885: add ss-lib-toggle

--- projects/test-app/src/app/app.imports.ts
+++ projects/test-app/src/app/app.imports.ts
@@ -1,7 +1,15 @@
-import { ButtonComponent, IconComponent, TextComponent } from '../../../front-components/src/lib/components';
+import {
+    ButtonComponent,
+    IconComponent,
+    TextComponent,
+    ToggleComponent
+} from '../../../front-components/src/lib/components';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export const appImports = [
     IconComponent,
     TextComponent,
-    ButtonComponent
+    ButtonComponent,
+    ToggleComponent,
+    ReactiveFormsModule
 ]
