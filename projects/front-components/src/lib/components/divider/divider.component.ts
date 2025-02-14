219b89f51ef7c1d58d02924bd26fa9e7ffbba3fa
RomanSH <Romqaa2021@yandex.ru>
2025-02-14 15:00:58 +0300

    feature/2899 sidebar and header

---
+++ projects/front-components/src/lib/components/divider/divider.component.ts
@@ -0,0 +1,32 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  InputSignal,
+  ViewEncapsulation,
+} from '@angular/core';
+import {DividerType} from '../../shared/models/enums/divider-type';
+
+
+/**
+ * Параметры:
+ *
+ * [direction]: 'vertical' | 'horizontal' - direction. По умолчанию: `horizontal`
+ */
+@Component({
+  selector: 'ss-lib-divider',
+  template: `
+    <div
+      class="divider"
+      [class.divider--horizontal]="direction() === 'horizontal'"
+      [class.divider--vertical]="direction() === 'vertical'"
+    ></div>
+  `,
+  styleUrls: ['divider.component.scss'],
+  standalone: true,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DividerComponent {
+  public direction: InputSignal<DividerType> = input<DividerType>(DividerType.Horizontal);
+}
