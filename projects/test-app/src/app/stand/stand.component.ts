ef5f78626fd315d5cef4875734ce50540e4c3d1a
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-02-27 13:28:22 +0300

    feature/2902: merge dev and fix conflicts

--- projects/test-app/src/app/stand/stand.component.ts
+++ projects/test-app/src/app/stand/stand.component.ts
@@ -8,7 +8,6 @@ import {
     IConfirmData,
     IconPosition,
     IconType,
-    IDictionaryItemDto,
     IModalData,
     LinkAppearance,
     Orientation,
@@ -16,22 +15,11 @@ import {
     Status,
     TextType,
     TextWeight,
-    IStoreTableBaseColumn,
 } from '../../../../front-components/src/lib/shared/models';
 import { standImports } from './stand.imports';
-import {ColumnsStateService} from '../../../../front-components/src/lib/components';
-
-export enum ExampleRowItemField {
-  example1 = 'example1',
-  example2 = 'example2',
-  example3 = 'example3',
-  example4 = 'example4',
-  example5 = 'example5',
-  example6 = 'example6',
-}
-
+import { ColumnsStateService, ConfirmComponent } from '../../../../front-components/src/lib/components';
 import { TestModalComponent } from '../test-modal/test-modal.component';
-import { ConfirmComponent } from '../../../../front-components/src/lib/components';
+import { DROPDOWN_ITEMS, DEFAULT_COLS } from './constants';
 
 @Component({
     selector: 'app-stand',
@@ -55,42 +43,17 @@ export class StandComponent {
     protected readonly Orientation = Orientation;
     protected readonly LinkAppearance = LinkAppearance;
     protected readonly Status = Status;
-    protected readonly dropdownItems: IDictionaryItemDto[] = [
-        {
-            id: 1,
-            name: 'option1'
-        },
-        {
-            id: 2,
-            name: 'option2'
-        },
-        {
-            id: 3,
-            name: 'option3'
-        },
-        {
-            id: 4,
-            name: 'option4'
-        },
-        {
-            id: 5,
-            name: 'option5'
-        },
-        {
-            id: 6,
-            name: 'option6'
-        },
-        {
-            id: 7,
-            name: 'option7'
-        }
-    ];
+    protected readonly DROPDOWN_ITEMS = DROPDOWN_ITEMS;
 
     toggleCtrl = new FormControl(false);
     inputCtrl = new FormControl('rrrr', [Validators.required, Validators.minLength(10)]);
     textareaCtrl = new FormControl('rrrr', [Validators.required, Validators.minLength(10)]);
     selectCtrl = new FormControl(null);
     numberPickerCtrl = new FormControl(2);
+
+    constructor(private readonly columnState: ColumnsStateService) {
+        this.columnState.colsTr$.next(DEFAULT_COLS)
+    }
 
     openModalWithComponent(): void {
         this.dialog.open<IModalData>(TestModalComponent, {
@@ -109,7 +72,6 @@ export class StandComponent {
         });
     }
 
-
     openConfirm(): void {
         this.dialog.open<IConfirmData>(ConfirmComponent, {
             data: {
@@ -124,120 +86,4 @@ export class StandComponent {
             } as IConfirmData
         });
     }
-
-  constructor(private readonly columnState: ColumnsStateService) {
-    this.columnState.colsTr$.next(this.defaultCols)
-
-  }
-
-  protected defaultCols: IStoreTableBaseColumn[] = [
-    {
-      id: ExampleRowItemField.example1,
-      title: '',
-      order: 0,
-      width: null,
-      skeleton: {
-        header: {
-          width: '100px',
-          height: '10px',
-          type: Shape.Round
-        },
-        body: {
-          width: '100px',
-          height: '100px',
-          type: Shape.Round
-        }
-      }
-    },
-    {
-      id: ExampleRowItemField.example2,
-      title: 'Порядок',
-      order: 1,
-      width: null,
-      skeleton: {
-        header: {
-          width: '100px',
-          height: '10px',
-          type: Shape.Square
-        },
-        body: {
-          width: '100px',
-          height: '20px',
-          type: Shape.Square
-        }
-      }
-    },
-    {
-      id: ExampleRowItemField.example3,
-      title: 'Изображение',
-      order: 2,
-      width: null,
-      skeleton: {
-        header: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        },
-        body: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        }
-      }
-    },
-    {
-      id: ExampleRowItemField.example4,
-      title: 'Баннер',
-      order: 3,
-      width: null,
-      skeleton: {
-        header: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        },
-        body: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        }
-      }
-    },
-    {
-      id: ExampleRowItemField.example5,
-      title: 'Статус',
-      order: 4,
-      width: null,
-      skeleton: {
-        header: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        },
-        body: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        }
-      }
-    },
-    {
-      id: ExampleRowItemField.example6,
-      title: 'Действие',
-      order: 5,
-      width: null,
-      skeleton: {
-        header: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        },
-        body: {
-          width: '100px',
-          height: '50px',
-          type: Shape.Square
-        }
-      }
-    }
-  ]
 }
