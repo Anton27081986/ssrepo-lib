6f67e24e4fa21c1c773c7a310abe97337dab0d43
RomanSH <Romqaa2021@yandex.ru>
2025-03-03 10:32:17 +0300

    update version + fix import

--- projects/front-components/src/lib/shared/models/interfaces/index.ts
+++ projects/front-components/src/lib/shared/models/interfaces/index.ts
@@ -1,14 +1,17 @@
-export { IStateElement } from './state-element';
-export { IMenu } from './menu';
-export { IDictionaryItemDto } from './dictionary-item-dto';
-export { PopoverContent } from './popover-content';
-export { IModalConfig } from './modal-config';
-export { IModalData, IConfirmData } from './modal-data';
-export { IBadgeProps } from './badge-props';
-export { SkeletonConf } from './skeleton.conf'
-export { IStoreTableBase } from './store-table-base-column'
-export { IStoreTableBaseColumn } from './store-table-base-column'
-export { ISkeletonTableBaseColumn } from './store-table-base-column'
-export { ISkeletonDerivativeThColumn } from './store-table-base-column'
-export { ISkeletonDerivativeTrTable } from './store-table-base-column'
-export { ISkeletonDerivativeTdTable } from './store-table-base-column'
+export {IStateElement} from './state-element';
+export {IMenu} from './menu';
+export {IDictionaryItemDto} from './dictionary-item-dto';
+export {PopoverContent} from './popover-content';
+export {IModalConfig} from './modal-config';
+export {IModalData, IConfirmData} from './modal-data';
+export {IBadgeProps} from './badge-props';
+export {SkeletonConf} from './skeleton.conf';
+export {
+  IStoreTableId,
+  IStoreTableBase,
+  IStoreTableBaseColumn,
+  ISkeletonTableBaseColumn,
+  ISkeletonDerivativeThColumn,
+  ISkeletonDerivativeTrTable,
+  ISkeletonDerivativeTdTable
+} from './store-table-base-column';
