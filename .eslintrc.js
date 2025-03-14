4cb00c2b2ed5903c17f9773f7f996b78aff5211a
as.borodulin <sacreder@gmail.com>
2025-03-14 17:10:38 +0300

    added class member ordering autofix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,114 +1,107 @@
 module.exports = {
 	root: true,
-	extends: ["plugin:storybook/recommended"],
-	ignorePatterns: [".cache/", ".git/", ".github/", "node_modules/"],
+	extends: ['plugin:storybook/recommended'],
+	ignorePatterns: ['.cache/', '.git/', '.github/', 'node_modules/'],
 	overrides: [
 		{
-			files: ["*.ts"],
+			files: ['*.ts'],
 			parserOptions: {
-				project: ["./tsconfig.json"],
-				createDefaultProgram: true
+				project: ['./tsconfig.json'],
+				createDefaultProgram: true,
 			},
+			plugins: ['sort-class-members'],
 			extends: [
-				"plugin:@angular-eslint/recommended",
-				"plugin:@typescript-eslint/recommended",
-				"@tinkoff/eslint-config/app",
-				"@tinkoff/eslint-config-angular",
-				"@tinkoff/eslint-config-angular/rxjs",
-				"@tinkoff/eslint-config-angular/promise",
-				"@tinkoff/eslint-config-angular/unicorn",
-				"@tinkoff/eslint-config-angular/html-eslint",
-				"@tinkoff/eslint-config-angular/file-progress",
-				"@tinkoff/eslint-config-angular/line-statements",
-				"@tinkoff/eslint-config-angular/member-ordering",
-				"@tinkoff/eslint-config-angular/decorator-position",
-				"@tinkoff/eslint-config-angular/function-return-type"
+				'plugin:@angular-eslint/recommended',
+				'plugin:@typescript-eslint/recommended',
+				'@tinkoff/eslint-config/app',
+				'@tinkoff/eslint-config-angular',
+				'@tinkoff/eslint-config-angular/rxjs',
+				'@tinkoff/eslint-config-angular/promise',
+				'@tinkoff/eslint-config-angular/unicorn',
+				'@tinkoff/eslint-config-angular/html-eslint',
+				'@tinkoff/eslint-config-angular/file-progress',
+				'@tinkoff/eslint-config-angular/line-statements',
+				'@tinkoff/eslint-config-angular/decorator-position',
+				'@tinkoff/eslint-config-angular/function-return-type',
 			],
 			rules: {
-				"import/no-unresolved": "off",
-				"import/extensions": "off",
-				"import/no-relative-packages": "off",
-				"import/no-cycle": "off",
-				"@typescript-eslint/ban-types": "off",
-				"@typescript-eslint/consistent-type-imports": "off",
-				"@typescript-eslint/no-empty-function": "off",
-				"@typescript-eslint/no-empty-object-type": "off",
-				"@typescript-eslint/no-explicit-any": "error",
-				"@typescript-eslint/explicit-member-accessibility": [
-					"warn",
+				'import/no-unresolved': 'off',
+				'import/extensions': 'off',
+				'import/no-relative-packages': 'off',
+				'import/no-cycle': 'off',
+				'@typescript-eslint/ban-types': 'off',
+				'@typescript-eslint/consistent-type-imports': 'off',
+				'@typescript-eslint/no-empty-function': 'off',
+				'@typescript-eslint/no-empty-object-type': 'off',
+				'@typescript-eslint/no-explicit-any': 'error',
+				'@typescript-eslint/explicit-member-accessibility': [
+					'warn',
 					{
-						accessibility: "explicit",
+						accessibility: 'explicit',
 						overrides: {
-							constructors: "no-public",
-							accessors: "explicit",
-							methods: "explicit",
-							properties: "explicit",
-							parameterProperties: "explicit"
-						}
-					}
+							constructors: 'no-public',
+							accessors: 'explicit',
+							methods: 'explicit',
+							properties: 'explicit',
+							parameterProperties: 'explicit',
+						},
+					},
 				],
-				"@typescript-eslint/member-ordering": [
-					"error",
-					{
-						"default": {
-							"memberTypes": [
-								// Статические члены
-								"public-static-field",    // Публичные статические свойства
-								"protected-static-field", // Защищённые статические свойства
-								"private-static-field",   // Приватные статические свойства
-
-								// Поля экземпляра
-								"public-field",           // Публичные поля
-								"protected-field",        // Защищённые поля
-								"private-field",          // Приватные поля
-
-								// Конструктор
-								"constructor",
-
-								"public-static-method",   // Публичные статические методы
-								"protected-static-method",// Защищённые статические методы
-								"private-static-method",  // Приватные статические методы
-
-								// Геттеры и сеттеры
-								"public-get",             // Публичные геттеры
-								"protected-get",          // Защищённые геттеры
-								"private-get",            // Приватные геттеры
-								"public-set",             // Публичные сеттеры
-								"protected-set",          // Защищённые сеттеры
-								"private-set",             // Приватные сеттеры
-
-								// Методы экземпляра
-								"public-method",          // Публичные методы
-								"protected-method",       // Защищённые методы
-								"private-method",         // Приватные методы
-							],
-							"order": "as-written"       // Порядок "как написано" в духе Airbnb
-						}
-					}
-				],
-				"no-useless-rename": [
-					"error",
+				'no-useless-rename': [
+					'error',
 					{
 						ignoreDestructuring: true,
 						ignoreImport: false,
-						ignoreExport: false
-					}
+						ignoreExport: false,
+					},
 				],
-				"no-useless-constructor": "off",
-				"no-useless-return": "off",
-				"no-console": [
-					"warn",
-					{ allow: ["info", "assert", "warn", "error"] }
-				]
-			}
+				'no-useless-constructor': 'off',
+				'no-useless-return': 'off',
+				'no-console': [
+					'warn',
+					{ allow: ['info', 'assert', 'warn', 'error'] },
+				],
+				'sort-class-members/sort-class-members': [
+					2,
+					{
+						order: [
+							'[static-fields]',
+							'[class-fields]',
+							'constructor',
+							'[static-methods]',
+							'[getter-setters]',
+							'[class-methods]',
+						],
+						groups: {
+							'static-fields': [
+								{ static: true, type:'property' }
+							],
+							'static-methods': [
+								{ static: true, type:'method' }
+							],
+							'class-fields': [
+								{ type: 'property' }
+							],
+							'getter-setters': [
+								{ kind: 'accessor'}
+							],
+							'class-methods': [
+								{ type: 'method' }
+							],
+
+						},
+						accessorPairPositioning: 'getThenSet',
+					},
+				],
+			},
 		},
 		{
-			files: ["*.html"],
-			extends: ["plugin:@angular-eslint/template/recommended"],
+			files: ['*.html'],
+			extends: ['plugin:@angular-eslint/template/recommended'],
 			rules: {
-				"prettier/prettier": ["error", { parser: "angular" }],
-				"@angular-eslint/template/attributes-order": ["error"]
-			}
-		}
-	]
+				'prettier/prettier': ['error', { parser: 'angular' }],
+				'@angular-eslint/template/attributes-order': ['error'],
+			},
+		},
+	],
 };
