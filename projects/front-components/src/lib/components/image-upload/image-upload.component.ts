291d0c8b3cea77dd27e391c69922456858ed00c0
Сергей Кондратьев <s.kandratsyeu@gmail.com>
2025-03-14 11:36:06 +0300

    image-attach

--- projects/front-components/src/lib/components/image-upload/image-upload.component.ts
+++ projects/front-components/src/lib/components/image-upload/image-upload.component.ts
@@ -1,15 +1,31 @@
 import {
 	ChangeDetectionStrategy,
 	Component,
+	forwardRef,
 	input,
-	output,
+	InputSignal,
 	signal,
 } from '@angular/core';
+import {
+	FormControl,
+	FormsModule,
+	NG_VALUE_ACCESSOR,
+	ReactiveFormsModule,
+} from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime, tap } from 'rxjs';
 import { TextComponent } from '../text/text.component';
 import { ButtonComponent, PreviewButtonComponent } from '../buttons';
-import { Colors, ExtraSize, IconType, TextType } from '../../shared/models';
+import {
+	Colors,
+	ExtraSize,
+	IconType,
+	TextType,
+	ToastTypeEnum,
+} from '../../shared/models';
 import { BadgeComponent } from '../badge/badge.component';
 import { SpinnerComponent } from '../spinner/spinner.component';
+import { SharedPopupService } from '../../shared/services';
 
 enum States {
 	Empty = 'empty',
@@ -17,6 +33,17 @@ enum States {
 	Preview = 'preview',
 }
 
+/**
+ * Параметры:
+ *
+ * [disabled]: boolean - Только для чтения. По умолчанию: `false`
+ *
+ * [maxSize]: number - Максимальный размер, байт. По умолчанию: `0`
+ *
+ * [maxHeight]: number - Минимальная высота, px. По умолчанию: `0`
+ *
+ * [maxWidth]: number - Максимальная ширина, px. По умолчанию: `0`
+ */
 @Component({
 	selector: 'ss-lib-image-upload',
 	templateUrl: './image-upload.component.html',
@@ -27,13 +54,27 @@ enum States {
 		BadgeComponent,
 		PreviewButtonComponent,
 		SpinnerComponent,
+		FormsModule,
+		ReactiveFormsModule,
 	],
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => ImageUploadComponent),
+			multi: true,
+		},
+	],
 })
 export class ImageUploadComponent {
 	public disabled = input<boolean>(false);
-	public imageChanged = output<File | null>();
+	public maxSize: InputSignal<number> = input<number>(0);
+
+	public maxHeight = input<number>(400);
+	public maxWidth = input<number>(400);
+
+	public inputCtrl = new FormControl();
 	protected hover = signal<boolean>(false);
 	protected state = signal<States>(States.Empty);
 	protected imageSrc = signal<ArrayBuffer | string | null>(null);
@@ -43,6 +84,46 @@ export class ImageUploadComponent {
 	protected readonly TextType = TextType;
 	protected readonly Colors = Colors;
 	protected readonly States = States;
+
+	private onChange!: (value: FileList | null) => void;
+	private onTouched!: () => void;
+
+	constructor(private readonly sharedPopupService: SharedPopupService) {
+		toSignal(
+			this.inputCtrl.valueChanges.pipe(
+				debounceTime(300),
+				tap<FileList | null>((value) => this.onChange(value)),
+			),
+		);
+
+		window.addEventListener(
+			'dragover',
+			function (e) {
+				e.preventDefault();
+			},
+			false,
+		);
+
+		window.addEventListener(
+			'drop',
+			function (e) {
+				e.preventDefault();
+			},
+			false,
+		);
+	}
+
+	public writeValue(value: FileList | null): void {
+		this.inputCtrl.setValue(value, { emitEvent: false });
+	}
+
+	public registerOnChange(fn: (value: FileList | null) => void): void {
+		this.onChange = fn;
+	}
+
+	public registerOnTouched(fn: () => FileList): void {
+		this.onTouched = fn;
+	}
 
 	protected onDragEnter(event: Event): void {
 		event.preventDefault();
@@ -71,38 +152,71 @@ export class ImageUploadComponent {
 		}
 	}
 
-	protected onChange(event: Event): void {
-		const inputElem: HTMLInputElement = event.target as HTMLInputElement;
+	protected onFileDelete(): void {
+		this.inputCtrl.setValue(null);
+		this.state.set(States.Empty);
+		this.imageSrc.set(null);
+	}
 
-		if (inputElem && inputElem.files && inputElem.files.length) {
-			this.onFileChange(inputElem.files);
+	protected selectFromPC(event: Event): void {
+		const inputCtrl = event.target as HTMLInputElement;
+
+		if (inputCtrl.files) {
+			this.onFileChange(inputCtrl.files);
 		}
 	}
 
-	protected onFileDelete(): void {
-		this.state.set(States.Empty);
-		this.imageSrc.set(null);
-		this.imageChanged.emit(null);
-	}
-
-	private onFileChange(files: FileList): void {
+	protected onFileChange(files: FileList): void {
 		if (this.disabled()) {
+			return;
+		}
+
+		const file = files[0];
+
+		if (!file) {
 			return;
 		}
 
 		this.state.set(States.Loading);
 
-		const file = files[0];
+		if (file.size > this.maxSize()) {
+			this.showToastError('Изображение не соответствует требованиям');
+			this.state.set(States.Empty);
 
-		const reader = new FileReader();
+			return;
+		}
 
-		reader.onload = () => {
-			this.state.set(States.Preview);
-			this.imageSrc.set(reader.result);
+		const img = new Image();
+
+		img.src = URL.createObjectURL(file);
+
+		img.onload = () => {
+			const height = img.height;
+			const width = img.width;
+
+			if (height > this.maxHeight() || width > this.maxWidth()) {
+				this.showToastError('Изображение не соответствует требованиям');
+				this.inputCtrl.setValue(null);
+				this.state.set(States.Empty);
+
+				return;
+			}
+
+			const reader = new FileReader();
+
+			reader.onload = () => {
+				this.state.set(States.Preview);
+				this.imageSrc.set(reader.result);
+			};
+
+			reader.readAsDataURL(file);
 		};
+	}
 
-		reader.readAsDataURL(file);
-
-		this.imageChanged.emit(files[0]);
+	private showToastError(text: string): void {
+		this.sharedPopupService.openToast({
+			text,
+			type: ToastTypeEnum.Error,
+		});
 	}
 }
