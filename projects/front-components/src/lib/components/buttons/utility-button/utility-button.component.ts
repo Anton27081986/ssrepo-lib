e22a1392075823d117dc2f2a3efe4929e3d9d569
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-03-25 15:31:40 +0300

    feature/3178: set button as active if dropdown opened

--- projects/front-components/src/lib/components/buttons/utility-button/utility-button.component.ts
+++ projects/front-components/src/lib/components/buttons/utility-button/utility-button.component.ts
@@ -1,11 +1,13 @@
 import {
 	ChangeDetectionStrategy,
 	Component,
+	inject,
 	input,
 	ViewEncapsulation,
 } from '@angular/core';
 import { BaseButtonComponent } from '../base-button/base-button.component';
 import { ButtonType, IconType } from '../../../shared/models';
+import { PopoverTriggerForDirective } from '../../../core/directives';
 
 /**
  * Параметры:
@@ -20,8 +22,10 @@ import { ButtonType, IconType } from '../../../shared/models';
 	imports: [BaseButtonComponent],
 	template: `
 		<ss-lib-base-button
+			[class.hovered]="this.popoverDirective?.isPopoverOpen() || false"
+			[isActive]="this.popoverDirective?.isPopoverOpen() || false"
 			[type]="type()"
-			[icon]="restrictedIcon()"
+			[icon]="icon()"
 			[iconPosition]="IconPosition.OnlyIcon"
 		>
 			<ng-content></ng-content>
@@ -32,8 +36,13 @@ import { ButtonType, IconType } from '../../../shared/models';
 	encapsulation: ViewEncapsulation.None,
 })
 export class UtilityButtonComponent extends BaseButtonComponent<ButtonType.Utility> {
+	public popoverDirective = inject(PopoverTriggerForDirective, {
+		optional: true,
+		self: true,
+	});
+
 	public override type = input<ButtonType.Utility>(ButtonType.Utility);
-	public restrictedIcon = input<IconType>(IconType.KebabMenuDots);
+	public override icon = input<IconType | null>(IconType.KebabMenuDots);
 
 	public readonly ButtonType = ButtonType;
 }
