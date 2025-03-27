790d076129b14253b80b2d1a51f742e2bc4cc9ab
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-03-27 18:38:20 +0300

    feature/3176: create story for datepicker

--- projects/front-components/src/lib/components/calendar/constans/min-year.ts
+++ projects/front-components/src/lib/components/calendar/constans/min-year.ts
@@ -1,4 +1,3 @@
-import { CalendarDay } from '../models';
 import { LIMIT_YEARS } from './limit-years';
 
-export const MIN_YEAR = CalendarDay.currentLocal().year - LIMIT_YEARS;
+export const MIN_YEAR = new Date().getFullYear() - LIMIT_YEARS;
--- projects/front-components/src/lib/shared/constants/index.ts
+++ projects/front-components/src/lib/shared/constants/index.ts
@@ -1,1 +1,3 @@
 export { EMPTY_STATE } from './empty-state';
+export { FIRST_DAY } from './first-day';
+export { LAST_DAY } from './last-day';
