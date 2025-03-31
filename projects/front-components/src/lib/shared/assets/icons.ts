09d42f31a86661f2e63d93700e331abd2999d96f
RomanSH <Romqaa2021@yandex.ru>
2025-03-31 14:33:17 +0300

    feature/checkbox checkbox

--- projects/front-components/src/lib/shared/assets/icons.ts
+++ projects/front-components/src/lib/shared/assets/icons.ts
@@ -285,4 +285,11 @@ export const ICONS = new Map<IconType, string>([
 			'<path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>' +
 			'</svg>',
 	],
+
+	[
+		IconType.Check,
+		'<svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+			'<path d="M20 6L9 17L4 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>' +
+			'</svg>',
+	],
 ]);
