67372510aa94141a8983476454fdc72a78615c97
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-04-28 10:04:06 +0300

    feature/carousel: set standalone to pagination

--- projects/front-components/src/lib/components/pagination-dots/pagination-dots.component.ts
+++ projects/front-components/src/lib/components/pagination-dots/pagination-dots.component.ts
@@ -8,6 +8,7 @@ import { RepeatTimesPipe } from '../../core/pipes';
 
 @Component({
 	selector: 'ss-lib-pagination-dots',
+	standalone: true,
 	imports: [RepeatTimesPipe],
 	templateUrl: './pagination-dots.component.html',
 	styleUrl: './pagination-dots.component.scss',
