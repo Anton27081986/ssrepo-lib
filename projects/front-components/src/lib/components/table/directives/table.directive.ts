4e0227313bfb37dbde6cf4d1fbb4163d28fa9b65
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-06-04 18:34:42 +0300

    feature/3353-table-styles: add params for border-radius/cell borders

--- projects/front-components/src/lib/components/table/directives/table.directive.ts
+++ projects/front-components/src/lib/components/table/directives/table.directive.ts
@@ -3,7 +3,13 @@ import { Directive, input } from '@angular/core';
 @Directive({
 	standalone: true,
 	selector: 'table[ssTable]',
+	host: {
+		'[class.has-border-radius]': 'hasBorderRadius()',
+		'[class.show-cell-borders]': 'showCellBorders()',
+	},
 })
 export class TableDirective<T extends Partial<Record<keyof T, never>>> {
+	public readonly hasBorderRadius = input<boolean>(true);
+	public readonly showCellBorders = input<boolean>(false);
 	public columns = input<ReadonlyArray<keyof T>>([]);
 }
