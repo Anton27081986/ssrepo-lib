021bca804d0a9b38a178633991b792e416d4a7ef
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-07-23 15:42:29 +0300

    feature/text-fix: add heading sizes

--- projects/front-components/src/lib/shared/models/enums/text-type.ts
+++ projects/front-components/src/lib/shared/models/enums/text-type.ts
@@ -1,5 +1,10 @@
 export enum TextType {
 	HeadingXs = 'heading-xs',
+	HeadingSm = 'heading-sm',
+	HeadingMd = 'heading-md',
+	HeadingLg = 'heading-lg',
+	HeadingXl = 'heading-xl',
+	Heading2Xl = 'heading-2xl',
 	BodyXs = 'body-xs',
 	BodySm = 'body-sm',
 	BodyMd = 'body-md',
